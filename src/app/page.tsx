130bdc31634b3ec199237bec34cf839e5b75ece5
Gaurav Panchal <gkp.125@gmail.com>
2024-07-14 01:34:58 +0530

    Step 2 Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,35 @@
-import Image from "next/image";
+import { Metadata } from "next";
 
+const posts = [
+  {
+    title: "My First Blog Post",
+    summary: "The first blog post I've written on my blogging software!",
+    post: "This is my first blog post content...",
+  },
+  {
+    title: "My Second Blog Post",
+    summary: "The Second blog post I've written on my blogging software!",
+    post: "This is my Second blog post content...",
+  },  {
+    title: "My Third Blog Post",
+    summary: "The Third blog post I've written on my blogging software!",
+    post: "This is my Third blog post content...",
+  },
+];
+
+export const metadata: Metadata = {
+  title: posts[0].post,
+  description: "Blogging Software Challenge #68",
+};
 export default function Home() {
   return (
     <main>
-      <h1>My First Blog</h1>
-      <p>The first blog post Ive written on my blogging software</p>
+      {posts.map((post) => (
+        <>
+          <h1>{post.title}</h1>
+          <p>{post.summary}</p>
+        </>
+      ))}
     </main>
   );
 }
